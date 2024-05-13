15275c3ec66ba606a1941abe88ae5727fb617d5b
nami-teis <nadiamarquez27112005@gmail.com>
2024-05-13 14:05:14 +0200

    Clase persona acabada

---
+++ Hogwarts/src/hogwarts/Casa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package hogwarts;
+
+/**
+ *
+ * @author a21nadiami
+ */
+public enum Casa {
+    GRYFFINDOR,
+    HUFFLEPUFF,
+    RAVENCLAW,
+    SLYTHERIN
+}
