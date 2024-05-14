0d3eab09fbd2868245ee69211d2495e3c7504824
nami-teis <nadiamarquez27112005@gmail.com>
2024-05-14 14:28:44 +0200

    Intento de arreglar cosas

--- Hogwarts/src/hogwarts/Hogwarts.java
+++ Hogwarts/src/hogwarts/Hogwarts.java
@@ -18,10 +18,42 @@ public class Hogwarts {
     
     public boolean anhadirAlumno(Mago alumno){
         Casa casa = casaAleatoria(); 
+        alumno.setCasa(casa);
+        if (!(alumnos.contains(alumno))){
+            alumnos.add(alumno); 
+            return true; 
+        } else {
+            return false; 
+        }
+    }
+    
+    public boolean graduar(Mago alumno){
+        if (alumnos.contains(alumno)){
+            alumnos.remove(alumno); 
+            return true; 
+        } else {
+            return false; 
+        }
+        
+    }
+    
+    /*Comprobar si ya esta asignado*/
+    public boolean asignarProfesor(String materia, Profesor profe){
+        profMateria.put(profe, materia); 
+        return false;
+        
     }
     
     private Casa casaAleatoria(){
         Casa[] valoresPosibles = Casa.values(); 
-        return valoresPosibles[Random.nextInt(valoresPosibles.length)]; 
+        Random r = new Random(); 
+        int valorAleatorio = r.nextInt(valoresPosibles.length);
+        return valoresPosibles[valorAleatorio]; 
+    }
+    
+    private void listarProfesores(){
+        for (Profesor profe : profMateria.keySet() ) {
+            profMateria[profe]; 
+        }
     }
 }
